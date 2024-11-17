8685b0e44f7042740db1fbce16e01de49ba3b853
Jeremy Grecu <jgrecu@gmail.com>
2024-11-17 16:15:19 +0100

    Implemented the ECHO command

--- cmd/main.go
+++ cmd/main.go
@@ -5,69 +5,113 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"log/slog"
 	"net"
-	"os"
+	"strings"
 )
 
-func main() {
-	run()
-}
+const ARRAY = '*'
+const BULK = '$'
 
-func run() {
+func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		slog.Error("failed to bind to port 6379: %v", err)
-		os.Exit(1)
+		log.Fatalf("failed to bind to port 6379: %v", err)
 	}
 
-	defer l.Close()
-
-	log.Printf("listening %v", l.Addr())
+	defer func(l net.Listener) {
+		err := l.Close()
+		if err != nil {
+			log.Fatal("failed closing the listener")
+		}
+	}(l)
+	log.Printf("listening on %v", l.Addr())
 
 	for {
 		// Block until we receive an incoming connection
 		conn, err := l.Accept()
 		if err != nil {
-			slog.Error("Error accepting connections: %w", err)
+			log.Printf("Error accepting connections: %v", err)
 			continue
 		}
 
 		// Handle client connection
-		go handleClient(conn)
+		go func() {
+			log.Println("accepted new connection")
+			handleStream(conn)
+		}()
 	}
 }
 
-func closeIt(c io.Closer, errp *error, msg string) {
-	err := c.Close()
-	if *errp == nil {
-		*errp = fmt.Errorf("%s: %w", msg, err)
-	}
-}
-
-func handleClient(conn net.Conn) {
+func handleStream(conn net.Conn) {
 	// Ensure we close the connection after we're done
-	defer conn.Close()
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalf("failed to close the connection: %v", err)
+		}
+	}(conn)
 
 	// Read data
-	buf := make([]byte, 1024)
+	buf := make([]byte, 128)
 	for {
-		n, err := conn.Read(buf)
-
+		// Read data from client
+		_, err := conn.Read(buf)
 		if err != nil {
 			if !errors.Is(err, io.EOF) {
-				slog.Error("reading error: %w", err)
+				log.Println(fmt.Errorf("reading error: %w", err))
 			}
-
 			return
 		}
+		//log.Printf("Message received: %s", buf)
+		response := evaluateResp(buf)
 
-		// Write the data back
-		_, err = conn.Write([]byte("+PONG\r\n"))
-		if err != nil {
-			slog.Error("writing error: %w", err)
+		_, err = conn.Write(response)
+	}
+}
+
+func evaluateResp(msg []byte) []byte {
+	var length = 0
+	if ARRAY == msg[0] {
+		length = int(msg[1]) - '0'
+
+		msg = msg[4:]
+		//log.Printf("it is an array: %s, %d", msg, length)
+	}
+	switch msg[0] {
+	case BULK:
+		//log.Printf("it is a bulk string: %s", msg)
+		args := processBulkStrings(msg, length) // if [echo, hello] or [ping]
+		log.Printf("commands: %v", args)
+		switch cmd := strings.ToLower(args[0]); {
+		case "ping" == cmd:
+			return appendSimpleString(msg[:0], "PONG")
+		case "echo" == cmd:
+			return appendSimpleString(msg[:0], args[1])
+		default:
+			return appendSimpleString(msg[:0], "-ERR unknown command")
 		}
 
-		slog.Info("Received %d bytes, message: %s", n, buf[:n])
+	default:
+		return appendSimpleString(msg[:0], "-ERR unknown command")
 	}
+
+}
+
+func processBulkStrings(msg []byte, length int) []string {
+	args := make([]string, 0, length)
+
+	for length > 0 {
+		argLen := int(msg[1]) - '0'
+		arg := string(msg[4 : argLen+4])
+		args = append(args, arg)
+
+		msg = msg[argLen+6:]
+
+		length--
+	}
+	return args
+}
+
+func appendSimpleString(b []byte, s string) []byte {
+	return fmt.Appendf(b, "+%s\r\n", s)
 }
