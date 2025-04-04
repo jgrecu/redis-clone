82d6e8b9b9e0de88236611eb18227bdc3dfc52ed
Jeremy Grecu <jgrecu@gmail.com>
2025-04-04 17:28:22 +0200

    RESET the applicsation, spart from scratch

--- app/main.go
+++ app/main.go
@@ -6,7 +6,12 @@ import (
 	"os"
 )
 
+// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
+var _ = net.Listen
+var _ = os.Exit
+
 func main() {
+
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
