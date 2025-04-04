c5f4585d9da8fcbaa4f1975d7c97d77df6bf8dd5
Jeremy Grecu <jgrecu@gmail.com>
2025-04-04 17:57:42 +0200

    Refactor key-value store to use a map with expiration time

---
+++ app/structures/redisDB.go
@@ -0,0 +1,86 @@
+package structures
+
+import (
+	"github.com/jgrecu/redis-clone/app/resp"
+	"strings"
+	"sync"
+	"time"
+)
+
+type mapValue struct {
+	Value  string
+	Expiry time.Time
+}
+
+var mapStore = make(map[string]mapValue, 0)
+var mut sync.RWMutex
+
+func Get(params []*resp.RESP) *resp.RESP {
+	if len(params) != 1 {
+		return &resp.RESP{
+			Type: "error",
+			Bulk: "ERR wrong number of arguments for 'get' command",
+		}
+	}
+
+	mut.RLock()
+	value, ok := mapStore[params[0].Bulk]
+	mut.RUnlock()
+
+	if !ok {
+		return &resp.RESP{
+			Type: "bulk",
+			Bulk: "-1",
+		}
+	}
+
+	if !value.Expiry.IsZero() && value.Expiry.Before(time.Now()) {
+		mut.Lock()
+		delete(mapStore, params[0].Bulk)
+		mut.Unlock()
+
+		return &resp.RESP{
+			Type: "bulk",
+			Bulk: "-1",
+		}
+	}
+
+	return &resp.RESP{
+		Type: "bulk",
+		Bulk: value.Value,
+	}
+}
+
+func Set(params []*resp.RESP) *resp.RESP {
+	if len(params) < 2 {
+		return &resp.RESP{
+			Type: "error",
+			Bulk: "ERR wrong number of arguments for 'set' command",
+		}
+	}
+
+	expirationDate := time.Time{}
+
+	if strings.ToUpper(params[2].Bulk) == "PX" {
+		expiry, err := time.ParseDuration(params[3].Bulk + "s")
+		if err != nil {
+			return &resp.RESP{
+				Type: "error",
+				Bulk: "ERR value is not an integer or out of range",
+			}
+		}
+		expirationDate = time.Now().Add(expiry)
+	}
+
+	mut.Lock()
+	mapStore[params[0].Bulk] = mapValue{
+		Value:  params[1].Bulk,
+		Expiry: expirationDate,
+	}
+	mut.Unlock()
+
+	return &resp.RESP{
+		Type: "string",
+		Bulk: "OK",
+	}
+}
