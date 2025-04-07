987873dcc6ef2579c49e10bf3e534eb7bb6c0065
Jeremy Grecu <jgrecu@gmail.com>
2025-04-07 13:27:13 +0200

    Update connection handling in replica.go

--- app/replica.go
+++ app/replica.go
@@ -1,28 +1,46 @@
 package main
 
 import (
+	"github.com/jgrecu/redis-clone/app/config"
 	"github.com/jgrecu/redis-clone/app/resp"
 	"net"
 )
 
 func handShake(link string) error {
+
+	send("PING")
+	send("REPLCONF", "listening-port", config.Get("port"))
+	send("REPLCONF", "capa", "psync2")
+
+	return nil
+}
+
+func send(commands ...string) error {
+	link := config.Get("master_host") + ":" + config.Get("master_port")
 	conn, err := net.Dial("tcp", link)
 	if err != nil {
 		return err
 	}
 	defer conn.Close()
 
-	// send ping
-	ping, _ := resp.RESP{
-		Type: "array",
-		Array: []resp.RESP{
-			{
-				Type: "bulk",
-				Bulk: "PING",
-			},
-		},
+	// send commands
+	commandsArray := make([]resp.RESP, len(commands))
+	for i, command := range commands {
+		commandsArray[i] = resp.RESP{
+			Type: "bulk",
+			Bulk: command,
+		}
+	}
+
+	message, err := resp.RESP{
+		Type:  "array",
+		Array: commandsArray,
 	}.Marshal()
 
-	conn.Write(ping)
+	if err != nil {
+		return err
+	}
+
+	conn.Write(message)
 	return nil
 }
