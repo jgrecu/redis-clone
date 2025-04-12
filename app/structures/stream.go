f5175b05287b36b763b94abdaa34c898d1dcb82f
Jeremy Grecu <jgrecu@gmail.com>
2025-04-12 15:20:00 +0200

    Refactor validateKey function in stream.go to fix index out of range error

--- app/structures/stream.go
+++ app/structures/stream.go
@@ -1,58 +1,58 @@
 package structures
 
 import (
-	"fmt"
-	"strings"
+    "fmt"
+    "strings"
 )
 
 type Entry struct {
-	Key   string
-	Pairs map[string]string
+    Key   string
+    Pairs map[string]string
 }
 
 type Stream struct {
-	Entries []Entry
+    Entries []Entry
 }
 
 func NewStream() *Stream {
-	return &Stream{Entries: []Entry{}}
+    return &Stream{Entries: []Entry{}}
 }
 
 func (s *Stream) Add(key string, pairs map[string]string) error {
-	if !s.validateKey(key) {
-		return fmt.Errorf("ERR The ID specified in XADD is equal or smaller than the target stream top item")
-	}
-	s.Entries = append(s.Entries, Entry{Key: key, Pairs: pairs})
-	return nil
+    if !s.validateKey(key) {
+        return fmt.Errorf("ERR The ID specified in XADD is equal or smaller than the target stream top item")
+    }
+    s.Entries = append(s.Entries, Entry{Key: key, Pairs: pairs})
+    return nil
 }
 
 func (s *Stream) Get(key string) (map[string]string, bool) {
-	for _, e := range s.Entries {
-		if e.Key == key {
-			return e.Pairs, true
-		}
-	}
-	return nil, false
+    for _, e := range s.Entries {
+        if e.Key == key {
+            return e.Pairs, true
+        }
+    }
+    return nil, false
 }
 
 func (s *Stream) validateKey(key string) bool {
-	ids := strings.Split(key, "-")
-	if ids[0] < "0" && ids[1] < "1" {
-		return false
-	}
+    ids := strings.Split(key, "-")
+    if ids[0] < "0" && ids[1] < "1" {
+        return false
+    }
 
-	len := len(s.Entries)
-	if len == 0 {
-		return true
-	}
+    len := len(s.Entries)
+    if len == 0 {
+        return true
+    }
 
-	lastIds := strings.Split(s.Entries[len].Key, "-")
+    lastIds := strings.Split(s.Entries[len-1].Key, "-")
 
-	if ids[0] < lastIds[0] {
-		return false
-	} else if ids[0] == lastIds[0] && ids[1] <= lastIds[1] {
-		return false
-	}
+    if ids[0] < lastIds[0] {
+        return false
+    } else if ids[0] == lastIds[0] && ids[1] <= lastIds[1] {
+        return false
+    }
 
-	return true
+    return true
 }
