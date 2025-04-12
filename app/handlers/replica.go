3fdc067ef5832aecdf4061926d72cda0993141ec
Jeremy Grecu <jgrecu@gmail.com>
2025-04-12 13:23:09 +0200

    Refactor replica.go to optimize waiting with buffered channel

--- app/handlers/replica.go
+++ app/handlers/replica.go
@@ -1,87 +1,85 @@
 package handlers
 
 import (
-	"encoding/hex"
-	"fmt"
-	"github.com/jgrecu/redis-clone/app/config"
-	"github.com/jgrecu/redis-clone/app/resp"
-	"log"
-	"strconv"
-	"time"
+    "encoding/hex"
+    "fmt"
+    "github.com/jgrecu/redis-clone/app/config"
+    "github.com/jgrecu/redis-clone/app/resp"
+    "log"
+    "strconv"
+    "time"
 )
 
 func replconf(params []resp.RESP) []byte {
-	if params[0].Bulk == "GETACK" {
-		return resp.Command("REPLCONF", "ACK", strconv.Itoa(config.Get().Offset)).Marshal()
-	}
-	return resp.String("OK").Marshal()
+    if params[0].Bulk == "GETACK" {
+        return resp.Command("REPLCONF", "ACK", strconv.Itoa(config.Get().Offset)).Marshal()
+    }
+    return resp.String("OK").Marshal()
 }
 
 func psync(params []resp.RESP) []byte {
-	valid := len(params) > 1 && params[0].Bulk == "?" && params[1].Bulk == "-1"
+    valid := len(params) > 1 && params[0].Bulk == "?" && params[1].Bulk == "-1"
 
-	if valid {
-		message := resp.String(
-			fmt.Sprintf("FULLRESYNC %s 0", config.Get().MasterReplId),
-		).Marshal()
+    if valid {
+        message := resp.String(
+            fmt.Sprintf("FULLRESYNC %s 0", config.Get().MasterReplId),
+        ).Marshal()
 
-		dbFile := getRDBFile()
-		message = append(message, []byte(fmt.Sprintf("$%d\r\n", len(dbFile)))...)
-		message = append(message, dbFile...)
-		return message
-	}
+        dbFile := getRDBFile()
+        message = append(message, []byte(fmt.Sprintf("$%d\r\n", len(dbFile)))...)
+        message = append(message, dbFile...)
+        return message
+    }
 
-	return resp.Error("Uncompleted command").Marshal()
+    return resp.Error("Uncompleted command").Marshal()
 }
 
 func getRDBFile() []byte {
-	file := "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
-	data, err := hex.DecodeString(file)
-	if err != nil {
-		return nil
-	}
-	return data
+    file := "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+    data, err := hex.DecodeString(file)
+    if err != nil {
+        return nil
+    }
+    return data
 }
 
 func wait(params []resp.RESP) []byte {
-	count, _ := strconv.Atoi(params[0].Bulk)
-	timeout, _ := strconv.Atoi(params[1].Bulk)
-	chanAck := make(chan bool)
-	ack := 0
-	for i := 0; i < len(config.Get().Replicas); i++ {
-		replica := config.Get().Replicas[i]
+    count, _ := strconv.Atoi(params[0].Bulk)
+    timeout, _ := strconv.Atoi(params[1].Bulk)
+    chanAck := make(chan int)
+    ack := 0
+    for i := 0; i < len(config.Get().Replicas); i++ {
+        replica := config.Get().Replicas[i]
 
-		if replica.GetOffset() > 0 {
-			go func(replica *config.Node) {
-				size, err := replica.Write(resp.Command("REPLCONF", "GETACK", "*").Marshal())
-				if err != nil {
-					log.Println("err REPLCONF: lost connection " + err.Error())
-				}
-				replica.AddOffset(size)
-				offset := <-replica.AckChan
-				log.Println("WAIT COMM REPLCONF: ", offset)
-				chanAck <- true
-			}(replica)
-		} else {
-			ack++
-		}
+        if replica.GetOffset() > 0 {
+            go func(replica *config.Node, ackChan chan<- int) {
+                size, err := replica.Write(resp.Command("REPLCONF", "GETACK", "*").Marshal())
+                if err != nil {
+                    log.Println("err REPLCONF: lost connection " + err.Error())
+                }
+                replica.AddOffset(size)
+                chanAck <- <-replica.AckChan
+                log.Println("WAIT COMM REPLCONF: ")
+            }(replica, chanAck)
+        } else {
+            ack++
+        }
 
-	}
+    }
 
-loop:
-	for ack > count {
-		// case timeout
-		select {
-		case <-chanAck:
-			ack++
-			log.Println("ack: ", ack)
-		case <-time.After(time.Duration(timeout) * time.Second):
-			break loop
-		}
-	}
+    for i := 0; i < count; i++ {
+        // case timeout
+        select {
+        case <-chanAck:
+            ack++
+            log.Println("ack: ", ack)
+        case <-time.After(time.Duration(timeout) * time.Millisecond):
+            i = count
+        }
+    }
 
-	return resp.Integer(ack).Marshal()
+    return resp.Integer(ack).Marshal()
 
-	//numberOfReplicas := len(config.Get().Replicas)
-	//return resp.Integer(numberOfReplicas).Marshal()
+    //numberOfReplicas := len(config.Get().Replicas)
+    //return resp.Integer(numberOfReplicas).Marshal()
 }
