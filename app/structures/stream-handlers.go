4942bd54de7c4e8ed73b43edc8f9f3cc68664156
Jeremy Grecu <jgrecu@gmail.com>
2025-04-12 17:41:43 +0200

    Refactor stream-handlers.go to improve readability and consistency

--- app/structures/stream-handlers.go
+++ app/structures/stream-handlers.go
@@ -1,125 +1,125 @@
 package structures
 
 import (
-	"github.com/jgrecu/redis-clone/app/resp"
-	"math"
-	"strconv"
+    "github.com/jgrecu/redis-clone/app/resp"
+    "math"
+    "strconv"
 )
 
 func Xadd(params []resp.RESP) []byte {
-	if len(params) < 2 {
-		return resp.Error("ERR wrong number of arguments for 'xadd' command").Marshal()
-	}
+    if len(params) < 2 {
+        return resp.Error("ERR wrong number of arguments for 'xadd' command").Marshal()
+    }
 
-	mut.Lock()
-	defer mut.Unlock()
+    mut.Lock()
+    defer mut.Unlock()
 
-	stream, ok := mapStore[params[0].Bulk]
-	entryKey := params[1].Bulk
+    stream, ok := mapStore[params[0].Bulk]
+    entryKey := params[1].Bulk
 
-	newMAp := make(map[string]string, 0)
-	for i := 2; i < len(params); i += 2 {
-		if i+1 < len(params) {
-			newMAp[params[i].Bulk] = params[i+1].Bulk
-		}
-	}
+    newMAp := make(map[string]string, 0)
+    for i := 2; i < len(params); i += 2 {
+        if i+1 < len(params) {
+            newMAp[params[i].Bulk] = params[i+1].Bulk
+        }
+    }
 
-	if !ok {
-		stream = MapValue{
-			Typ:    "stream",
-			Stream: NewStream(),
-		}
-	}
+    if !ok {
+        stream = MapValue{
+            Typ:    "stream",
+            Stream: NewStream(),
+        }
+    }
 
-	key, err := stream.Stream.Add(entryKey, newMAp)
-	if err != nil {
-		return resp.Error(err.Error()).Marshal()
-	}
+    key, err := stream.Stream.Add(entryKey, newMAp)
+    if err != nil {
+        return resp.Error(err.Error()).Marshal()
+    }
 
-	mapStore[params[0].Bulk] = stream
+    mapStore[params[0].Bulk] = stream
 
-	return resp.Bulk(key).Marshal()
+    return resp.Bulk(key).Marshal()
 }
 
 func XRange(params []resp.RESP) []byte {
-	if len(params) < 3 {
-		return resp.Error("ERR wrong number of arguments for 'xrange' command").Marshal()
-	}
+    if len(params) < 3 {
+        return resp.Error("ERR wrong number of arguments for 'xrange' command").Marshal()
+    }
 
-	mut.RLock()
-	defer mut.RUnlock()
+    mut.RLock()
+    defer mut.RUnlock()
 
-	value, ok := mapStore[params[0].Bulk]
-	if !ok || value.Typ != "stream" {
-		return resp.Nil().Marshal()
-	}
+    value, ok := mapStore[params[0].Bulk]
+    if !ok || value.Typ != "stream" {
+        return resp.Nil().Marshal()
+    }
 
-	start := params[1].Bulk
-	end := params[2].Bulk
+    start := params[1].Bulk
+    end := params[2].Bulk
 
-	if start == "-" {
-		start = "0-0"
-	}
+    if start == "-" {
+        start = "0-0"
+    }
 
-	if end == "+" {
-		end = strconv.Itoa(math.MaxInt) + "-" + strconv.Itoa(math.MaxInt)
-	}
+    if end == "+" {
+        end = strconv.Itoa(math.MaxInt) + "-" + strconv.Itoa(math.MaxInt)
+    }
 
-	entries := value.Stream.Range(start, end)
+    entries := value.Stream.Range(start, end)
 
-	res := []resp.RESP{}
-	for _, entry := range entries {
-		pairs := []resp.RESP{}
-		for k, v := range entry.Pairs {
-			pairs = append(pairs, resp.Bulk(k), resp.Bulk(v))
-		}
+    res := []resp.RESP{}
+    for _, entry := range entries {
+        pairs := []resp.RESP{}
+        for k, v := range entry.Pairs {
+            pairs = append(pairs, resp.Bulk(k), resp.Bulk(v))
+        }
 
-		res = append(res, resp.Array(resp.Bulk(entry.Key()), resp.Array(pairs...)))
-	}
+        res = append(res, resp.Array(resp.Bulk(entry.Key()), resp.Array(pairs...)))
+    }
 
-	return resp.Array(res...).Marshal()
+    return resp.Array(res...).Marshal()
 }
 
 func XRead(params []resp.RESP) []byte {
-	if len(params)%2 != 1 {
-		return resp.Error("ERR wrong number of arguments for 'xread' command").Marshal()
-	}
+    if len(params)%2 != 1 {
+        return resp.Error("ERR wrong number of arguments for 'xread' command").Marshal()
+    }
 
-	mut.RLock()
-	defer mut.RUnlock()
+    mut.RLock()
+    defer mut.RUnlock()
 
-	streams := make([]resp.RESP, 0)
+    streams := make([]resp.RESP, 0)
 
-	streamKeyLen := (len(params)-1)/2 + 1
-	for i := 1; i < streamKeyLen+1; i++ {
-		streamKey := params[i].Bulk
-		val, ok := mapStore[streamKey]
-		if !ok || val.Typ != "stream" {
-			continue
-		}
+    streamLen := (len(params) - 1) / 2
+    for i := 1; i < streamLen+1; i++ {
+        streamKey := params[i].Bulk
+        val, ok := mapStore[streamKey]
+        if !ok || val.Typ != "stream" {
+            continue
+        }
 
-		stream := val.Stream
+        stream := val.Stream
 
-		startKey := params[i+streamKeyLen].Bulk
-		entries := stream.Read(startKey)
-		for _, entry := range entries {
-			pairs := []resp.RESP{}
-			for k, v := range entry.Pairs {
-				pairs = append(pairs, resp.Bulk(k), resp.Bulk(v))
-			}
+        startKey := params[i+streamLen].Bulk
+        entries := stream.Read(startKey)
+        for _, entry := range entries {
+            pairs := []resp.RESP{}
+            for k, v := range entry.Pairs {
+                pairs = append(pairs, resp.Bulk(k), resp.Bulk(v))
+            }
 
-			streams = append(
-				streams,
-				resp.Array(
-					resp.Bulk(streamKey),
-					resp.Array(
-						resp.Bulk(entry.Key()),
-						resp.Array(pairs...),
-					),
-				),
-			)
-		}
-	}
+            streams = append(
+                streams,
+                resp.Array(
+                    resp.Bulk(streamKey),
+                    resp.Array(
+                        resp.Bulk(entry.Key()),
+                        resp.Array(pairs...),
+                    ),
+                ),
+            )
+        }
+    }
 
-	return resp.Array(streams...).Marshal()
+    return resp.Array(streams...).Marshal()
 }
