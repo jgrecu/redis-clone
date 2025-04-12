98feee5c60c076b9c99861bd007d565c6431f77e
Jeremy Grecu <jgrecu@gmail.com>
2025-04-12 14:47:08 +0200

    Add error handling to XADD function in redisMap.go

--- app/structures/stream.go
+++ app/structures/stream.go
@@ -1,5 +1,10 @@
 package structures
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Entry struct {
 	Key   string
 	Pairs map[string]string
@@ -13,8 +18,12 @@ func NewStream() *Stream {
 	return &Stream{Entries: []Entry{}}
 }
 
-func (s *Stream) Add(key string, pairs map[string]string) {
+func (s *Stream) Add(key string, pairs map[string]string) error {
+	if !s.validateKey(key) {
+		return fmt.Errorf("ERR The ID specified in XADD is equal or smaller than the target stream top item")
+	}
 	s.Entries = append(s.Entries, Entry{Key: key, Pairs: pairs})
+	return nil
 }
 
 func (s *Stream) Get(key string) (map[string]string, bool) {
@@ -24,4 +33,21 @@ func (s *Stream) Get(key string) (map[string]string, bool) {
 		}
 	}
 	return nil, false
+}
+
+func (s *Stream) validateKey(key string) bool {
+	ids := strings.Split(key, "-")
+	if ids[0] < "0" && ids[1] < "1" {
+		return false
+	}
+
+	lastIds := strings.Split(s.Entries[len(s.Entries)-1].Key, "-")
+
+	if ids[0] < lastIds[0] {
+		return false
+	} else if ids[0] == lastIds[0] && ids[1] <= lastIds[1] {
+		return false
+	}
+
+	return true
 }
