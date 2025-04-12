7618e181518e4de4bed4477c75f0a95f0b37cdd0
Jeremy Grecu <jgrecu@gmail.com>
2025-04-12 15:23:43 +0200

    Refactor validateKey function in stream.go to improve error handling and fix index out of range issue

--- app/structures/stream.go
+++ app/structures/stream.go
@@ -19,9 +19,11 @@ func NewStream() *Stream {
 }
 
 func (s *Stream) Add(key string, pairs map[string]string) error {
-    if !s.validateKey(key) {
-        return fmt.Errorf("ERR The ID specified in XADD is equal or smaller than the target stream top item")
+    err := s.validateKey(key)
+    if err != nil {
+        return err
     }
+
     s.Entries = append(s.Entries, Entry{Key: key, Pairs: pairs})
     return nil
 }
@@ -35,24 +37,22 @@ func (s *Stream) Get(key string) (map[string]string, bool) {
     return nil, false
 }
 
-func (s *Stream) validateKey(key string) bool {
+func (s *Stream) validateKey(key string) error {
     ids := strings.Split(key, "-")
     if ids[0] < "0" && ids[1] < "1" {
-        return false
+        return fmt.Errorf("ERR The ID specified in XADD must be greater than 0-0")
     }
 
     len := len(s.Entries)
     if len == 0 {
-        return true
+        return nil
     }
 
     lastIds := strings.Split(s.Entries[len-1].Key, "-")
 
-    if ids[0] < lastIds[0] {
-        return false
-    } else if ids[0] == lastIds[0] && ids[1] <= lastIds[1] {
-        return false
+    if (ids[0] < lastIds[0]) || (ids[0] == lastIds[0] && ids[1] <= lastIds[1]) {
+        return fmt.Errorf("ERR The ID specified in XADD is smaller than the target stream top item")
     }
 
-    return true
+    return nil
 }
