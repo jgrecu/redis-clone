a6b25cf51a2751a686bd8df8a46e17a9cec270e0
Jeremy Grecu <jgrecu@gmail.com>
2025-04-12 13:56:06 +0200

    Refactor replica.go to use goroutines for sending acknowledgments

--- app/config/node.go
+++ app/config/node.go
@@ -1,79 +1,81 @@
 package config
 
 import (
-	"bufio"
-	"github.com/jgrecu/redis-clone/app/resp"
-	"log"
-	"net"
-	"sync"
+    "bufio"
+    "github.com/jgrecu/redis-clone/app/resp"
+    "log"
+    "net"
+    "sync"
 )
 
 type Node struct {
-	Conn    net.Conn
-	Reader  *resp.RespReader
-	offset  int
-	id      string
-	mu      sync.Mutex
-	AckChan []chan<- int
+    Conn    net.Conn
+    Reader  *resp.RespReader
+    offset  int
+    id      string
+    mu      sync.Mutex
+    AckChan []chan<- int
 }
 
 func NewNode(conn net.Conn) *Node {
-	return &Node{
-		Conn:    conn,
-		Reader:  resp.NewRespReader(bufio.NewReader(conn)),
-		offset:  0,
-		id:      conn.RemoteAddr().String(),
-		mu:      sync.Mutex{},
-		AckChan: make([]chan<- int, 1),
-	}
+    return &Node{
+        Conn:    conn,
+        Reader:  resp.NewRespReader(bufio.NewReader(conn)),
+        offset:  0,
+        id:      conn.RemoteAddr().String(),
+        mu:      sync.Mutex{},
+        AckChan: make([]chan<- int, 1),
+    }
 }
 
 func (n *Node) Close() {
-	n.Conn.Close()
+    n.Conn.Close()
 }
 
 func (n *Node) AddOffset(offset int) {
-	n.mu.Lock()
-	defer n.mu.Unlock()
-	n.offset += offset
+    n.mu.Lock()
+    defer n.mu.Unlock()
+    n.offset += offset
 }
 
 func (n *Node) GetOffset() int {
-	n.mu.Lock()
-	defer n.mu.Unlock()
-	return n.offset
+    n.mu.Lock()
+    defer n.mu.Unlock()
+    return n.offset
 }
 
 func (n *Node) Read() (resp.RESP, error) {
-	return n.Reader.Read()
+    return n.Reader.Read()
 }
 
 func (n *Node) ReadRDB() (resp.RESP, error) {
-	return n.Reader.ReadRDB()
+    return n.Reader.ReadRDB()
 }
 
 func (n *Node) Write(data []byte) (int, error) {
-	return n.Conn.Write(data)
+    return n.Conn.Write(data)
 }
 
 func (n *Node) SendAck(ack chan<- int) (int, error) {
-	n.mu.Lock()
-	n.AckChan = append(n.AckChan, ack)
-	n.mu.Unlock()
-	log.Println("Sending from replica : ", n.id)
-	return n.Conn.Write(
-		resp.Command("REPLCONF", "GETACK", "*").Marshal(),
-	)
+    n.mu.Lock()
+    n.AckChan = append(n.AckChan, ack)
+    defer n.mu.Unlock()
+    log.Println("Sending from replica : ", n.id)
+    return n.Conn.Write(
+        resp.Command("REPLCONF", "GETACK", "*").Marshal(),
+    )
 }
 
 func (n *Node) ReceiveAck(offset int) {
-	log.Println("Received ack from replica : ", n.id)
-	n.mu.Lock()
-	defer n.mu.Unlock()
-	if len(n.AckChan) == 0 {
-		return
-	}
-	ch := n.AckChan[0]
-	n.AckChan = n.AckChan[1:]
-	ch <- offset
+    log.Println("Received ack from replica : ", n.id)
+    n.mu.Lock()
+    defer n.mu.Unlock()
+    if len(n.AckChan) == 0 {
+        return
+    }
+    ch := n.AckChan[0]
+    n.AckChan = n.AckChan[1:]
+    ch <- offset
+
+    log.Println("Receive func: Ack sent through the channel")
 }
